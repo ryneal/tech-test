649f88a7ab90b8df87f5eb597750d3e6abbd2d9d
ryan <ryandavidneal@gmail.com>
2017-07-08 18:42:30 +0100

    * Done some work setting up the views, having a small problem with the fragment and setting titles

--- src/main/java/com/github/ryneal/techtest/service/PersonService.java
+++ src/main/java/com/github/ryneal/techtest/service/PersonService.java
@@ -4,12 +4,13 @@ import com.github.ryneal.techtest.model.Person;
 import org.springframework.stereotype.Service;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class PersonService {
     public List<Person> getPeople() {
-        return null;
+        return Collections.emptyList();
     }
 
     public Person save(Person person) {
