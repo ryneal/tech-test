c9770a4ef85cd9dbcd91d6046a70b0e9489e5045
ryan <ryandavidneal@gmail.com>
2017-07-08 16:24:24 +0100

    * Created some tests for the view, but are purposely failing

--- src/test/java/com/github/ryneal/techtest/TechTestApplicationTests.java
+++ src/test/java/com/github/ryneal/techtest/TechTestApplicationTests.java
@@ -2,15 +2,52 @@ package com.github.ryneal.techtest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 public class TechTestApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
 	@Test
-	public void contextLoads() {
+	public void shouldBeAbleToAccessPeoplePage() throws Exception {
+		ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody()).contains("<title>People</title>");
+	}
+
+	@Test
+	public void shouldBeAbleToAccessCreatePage() throws Exception {
+		throw new NotImplementedException();
+	}
+
+	@Test
+	public void shouldBeAbleToAccessUpdatePersonPage() throws Exception {
+		throw new NotImplementedException();
+	}
+
+	@Test
+	public void shouldBeAbleToAccessBootstrapCss() throws Exception {
+		ResponseEntity<String> entity = this.restTemplate.getForEntity(
+				"http://localhost:" + this.port + "/css/bootstrap.min.css", String.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(entity.getBody()).contains("body");
 	}
 
 }
---
+++ src/test/java/com/github/ryneal/techtest/controller/PersonControllerTest.java
@@ -0,0 +1,22 @@
+package com.github.ryneal.techtest.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+
+public class PersonControllerTest {
+
+
+
+}
